cda11b629df4a187fe1b405e0c415cdea23c559c
Raushan <raushankumar606@gmail.com>
2020-07-30 14:54:11 +0530

    mongoose

--- models/dishes.js
+++ models/dishes.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency;
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema(
@@ -34,6 +36,28 @@ const dishSchema = new Schema(
       type: String,
       required: true,
     },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+
     comments: [commentSchema],
   },
   {
